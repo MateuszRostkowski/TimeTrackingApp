7f461f93e5e949e7429d1338b39d276e074e3c71
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-02-07 10:29:52 +0100

    add simple styled components to app component

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react'
+import { SafeAreaView, View, ScrollView, Text } from 'react-native'
+import styled from 'styled-components/native'
 
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+// import EditableTimer from "./components/EditableTimer"
+// import ToggleableTimerForm from "./components/ToggleableTimerForm"
+
+const AppContainer = styled.SafeAreaView`
+  flex: 1;
+`
+
+const TitleContainer = styled.View`
+  padding-top: 35px;
+  padding-bottom: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: #d6d7da;
+`
+
+const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`
+
+const TimerList = styled.ScrollView`
+  padding-bottom: 15px;
+`
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-    </SafeAreaView>
-  );
-};
+    <AppContainer>
+      <TitleContainer>
+        <Title>Timers</Title>
+      </TitleContainer>
+      <TimerList>
+        <Text>Here would be list of timmers :D</Text>
+      </TimerList>
+    </AppContainer>
+  )
+}
 
-export default App;
+export default App
