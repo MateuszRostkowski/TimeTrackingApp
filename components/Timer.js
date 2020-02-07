654566af0536c0304df10f8723418fa46cd1f886
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-02-07 12:53:17 +0100

    add styled components to Timer component

--- components/Timer.js
+++ components/Timer.js
@@ -1,6 +1,50 @@
 import React from 'react'
+import styled from 'styled-components/native'
 import { Text } from 'react-native'
 
-const Timer = () => <Text>Hello from Timer</Text>
+import TimerButton from './TimerButton'
+import { millisecondsToHuman } from '../utils/TimerUtils'
+
+const TimerContainer = styled.View`
+  background-color: #fff;
+  border: 2px solid #d6d7da;
+  border-radius: 10px;
+  padding: 15px;
+  margin: 15px;
+  margin-bottom: 0;
+`
+
+const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+`
+
+const ElapsedTime = styled.Text`
+  font-size: 26px;
+  font-weight: bold;
+  text-align: center;
+  padding: 0 15px;
+`
+
+const ButtonGroup = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const Timer = ({ title, project, elapsed }) => {
+  const elapsedString = millisecondsToHuman(elapsed)
+
+  return (
+    <TimerContainer>
+      <Title>{title}</Title>
+      <Text>{project}</Text>
+      <ElapsedTime>{elapsedString}</ElapsedTime>
+      <ButtonGroup>
+        <TimerButton color="blue" small title="Edit" />
+        <TimerButton color="blue" small title="Remove" />
+      </ButtonGroup>
+    </TimerContainer>
+  )
+}
 
 export default Timer
